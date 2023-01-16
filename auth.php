7096bb13fbff3aaa46e25d5f7244ddb8c0e29340
h-takahashi-git <takahashishigoto@gmail.com>
2023-01-16 14:23:51 +0900

    test

--- auth.php
+++ auth.php
@@ -2,8 +2,19 @@
 
 function createMyAccount() {
 
+
+    $id = 1;
+    mysql_connect('localhost', $username, $password) or die('Could not connect: ' . mysql_error());
+    mysql_select_db('myDatabase') or die('Could not select database');
+
+    $result = mysql_query("SELECT * FROM myTable WHERE id = " . $id);
+
+    while ($row = mysql_fetch_object($result)) {
+        echo $row->name;
+    }
+
     $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-    socket_connect($socket, '8.8.8.8', 23);  // Sensitive
+    socket_connect($socket, '8.8.8.8', 23); 
 
 
     $password = "65DBGgwe4uazdWQA";
--- index.php
+++ index.php
@@ -4,5 +4,9 @@ use Symfony\Component\HttpFoundation\Response;
 
 public function index(Request $request)
 {
+
+    $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+    socket_connect($socket, '8.8.8.8', 23); 
+
     return $request;
 }
