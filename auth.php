cc0fe1f9fc92fa00ec13e2ef4fe864cadaf41fda
h-takahashi-git <takahashishigoto@gmail.com>
2023-01-16 14:20:56 +0900

    test

--- auth.php
+++ auth.php
@@ -1,6 +1,11 @@
 <?php
 
 function createMyAccount() {
+
+    $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+    socket_connect($socket, '8.8.8.8', 23);  // Sensitive
+
+
     $password = "65DBGgwe4uazdWQA";
 
     $httpUrl = "https://example.domain?user=user&password=65DBGgwe4uazdWQA"
